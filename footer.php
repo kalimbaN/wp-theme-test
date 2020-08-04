eb8a140b70bfd7dfcc37d6ab38c475d0b997ec60
kalimbaN <kalimban@gmail.com>
2020-08-04 23:52:27 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 
 	<footer>
 		<h5><?php echo esc_html( Theme_Utils::instance()->footer_text() ); ?></h5>
+		<bold><?php echo esc_html__( 'This is Kapp theme Version 1.0.0' ); ?></bold>
 		<p><?php echo esc_html__( 'Kapp &copy; 2020', 'kapp-test-theme' ); ?></p>
 	</footer>
 	<?php wp_footer(); ?>
